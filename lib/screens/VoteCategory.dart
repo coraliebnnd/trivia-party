934d072642c7b34969baec59e290161b131c94d4
Niklas Zimmer <nzimmer@uni-bremen.de>
2024-11-25 18:10:51 +0100

    Fixed styling a bit

--- lib/screens/VoteCategory.dart
+++ lib/screens/VoteCategory.dart
@@ -44,7 +44,7 @@ class VoteCategory extends StatelessWidget {
                 // Categories
                 Expanded(
                   child: GridView.count(
-                    crossAxisCount: 3,
+                    crossAxisCount: 2,
                     mainAxisSpacing: 10,
                     crossAxisSpacing: 10,
                     childAspectRatio: 2.5,
