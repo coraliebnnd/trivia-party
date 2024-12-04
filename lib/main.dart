fcb91b638403efe7de4e969ad2c14629d7d0d637
Niklas Eckert <niklas.eckert.1807@gmail.com>
2024-12-04 14:59:38 +0100

    #1: Added basic firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,17 @@ import 'package:trivia_party/screens/HowToPlay.dart';
 import 'package:trivia_party/screens/Question.dart';
 import 'package:trivia_party/screens/VoteCategory.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'Routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     BlocProvider(
       create: (context) => GameBloc(),
