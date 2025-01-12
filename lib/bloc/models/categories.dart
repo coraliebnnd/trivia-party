21b66a981a0e82b314dcbef8cdc52a2ae2a76a6d
Niklas Zimmer <nzimmer@uni-bremen.de>
2025-01-12 13:00:58 +0100

    #26: Added button for selecting random category

--- lib/bloc/models/categories.dart
+++ lib/bloc/models/categories.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+int RANDOM_ID = 7;
 final Map<int, Category> categories = {
   1: Category(1, [25], "Art", Colors.pink),
   2: Category(2, [15], "Video Games", Colors.purple),
@@ -7,6 +8,7 @@ final Map<int, Category> categories = {
   4: Category(4, [21], "Sport", Colors.orange),
   5: Category(5, [12], "Music", Colors.yellow),
   6: Category(6, [10], "Books", Colors.green),
+  RANDOM_ID: Category(RANDOM_ID, [-1], "Random", Colors.grey)
 };
 
 void resetCategoryVotes() {
